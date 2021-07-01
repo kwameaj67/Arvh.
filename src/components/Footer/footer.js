ee1aeda0dc53f801a611429ae1d09d178a1e8e20
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-01 09:30:24 +0000

    footer complete

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,26 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <>
+      <section className="footer">
+        <div className="footer_container">
+          <div className="logo">
+            <a href="/">Arvh.</a>
+          </div>
+          <div className="footer_links_container">
+            <ul className="footer_links">
+              <li><a href="/" id="about">About Us</a></li>
+              <li><a href="/" id="about">How it Works</a></li>
+              <li><a href="/" id="services">FAQ</a></li>
+              <li><a href="/" id="projects">Contact Us</a></li>
+              <li><a href="/" id="projects">Privacy Policy</a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Footer;
