5635db16476c13202a0d73113a378d77b6771944
Kwame <kwameagyenimboateng64@gmail.com>
2021-07-02 12:21:13 +0000

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/navbar';
+import Section from './components/Sections/section'
+import Footer from './components/Footer/footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navbar/>
+      <Section/>
+      <Footer/>
     </div>
   );
 }
